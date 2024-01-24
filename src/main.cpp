bc807a8538c8ca1ca977b54375e0849388fd625d
Tom Sarry <tom.sarry@mail.mcgill.ca>
2024-01-23 22:18:28 -0500

    feat: add color helper

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,23 @@
 #include <iostream>
 
+#include "utils/color.h"
+
 int main() {
-    int image_width = 256;
-    int image_height = 256;
+	int image_width = 256;
+	int image_height = 256;
 
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = 0; j < image_height; ++j) {
-        std::clog << "\rScanlines remanaining: " << (image_height-j) << ' ' << std::flush;
-        for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0;
+	for (int j = 0; j < image_height; ++j) {
+		std::clog << "\rScanlines remanaining: " << (image_height - j) << ' '
+				  << std::flush;
+		for (int i = 0; i < image_width; ++i) {
+			auto pixel_color = color(
+				double(i) / (image_width - 1), double(j) / (image_height - 1),
+				0);
+			write_color(std::cout, pixel_color);
+		}
+	}
 
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
-        }
-    }
-
-    std::clog << "\rDone.                   " << std::endl;
+	std::clog << "\rDone.                   " << std::endl;
 }
---
+++ src/utils/color.h
@@ -0,0 +1,16 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include <iostream>
+
+#include "vec3.h"
+
+using color = vec3;
+
+inline void write_color(std::ostream& out, color pixel_color) {
+	out << static_cast<int>(255.999 * pixel_color.x()) << ' '
+		<< static_cast<int>(255.999 * pixel_color.y()) << ' '
+		<< static_cast<int>(255.999 * pixel_color.z()) << '\n';
+}
+
+#endif
